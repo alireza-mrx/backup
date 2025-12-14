80b1a5a68618e67dfdaeb1d41b593fda6bc7fd54
alireza-mrx <161974832+alireza-mrx@users.noreply.github.com>
2025-12-14 15:09:56 +0330

    backup.sh

--- backup.sh
+++ backup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
+# --- Input Validation ---
 
 while [[ -z "$tk" ]]; do
     echo "Bot token: "
     read -r tk
-    if [[ $tk == $'\0' ]]; then
-        echo "Invalid input. Token cannot be empty."
+    if [[ -z "$tk" ]]; then
+        echo "Token cannot be empty. Please enter the full bot token."
         unset tk
     fi
 done
 
-
 while [[ -z "$chatid" ]]; do
     echo "Chat id: "
     read -r chatid
-    if [[ $chatid == $'\0' ]]; then
-        echo "Invalid input. Chat id cannot be empty."
+    if [[ -z "$chatid" ]]; then
+        echo "Chat id cannot be empty."
         unset chatid
     elif [[ ! $chatid =~ ^\-?[0-9]+$ ]]; then
         echo "${chatid} is not a number."
@@ -23,41 +23,52 @@ while [[ -z "$chatid" ]]; do
     fi
 done
 
-
 echo "Caption (for example, your domain, to identify the database file more easily): "
 read -r caption
 
+# --- Cronjob Logic (Improved for readability and accuracy) ---
 
 while true; do
     echo "Cronjob (minutes and hours) (e.g : 30 6 or 0 12) : "
     read -r minute hour
-    if [[ $minute == 0 ]] && [[ $hour == 0 ]]; then
-        cron_time="0 0 * * *"
-        break
-    elif [[ $minute == 0 ]] && [[ $hour =~ ^[0-9]+$ ]] && [[ $hour -lt 24 ]]; then
-        cron_time="0 */${hour} * * *"
-        break
-    elif [[ $hour == 0 ]] && [[ $minute =~ ^[0-9]+$ ]] && [[ $minute -lt 60 ]]; then
-        cron_time="*/${minute} * * * *"
-        break
-    elif [[ $minute =~ ^[0-9]+$ ]] && [[ $hour =~ ^[0-9]+$ ]] && [[ $hour -lt 24 ]] && [[ $minute -lt 60 ]]; then
-        cron_time="*/${minute} */${hour} * * *"
-        break
+
+    minute=$(echo "$minute" | xargs)
+    hour=$(echo "$hour" | xargs)
+
+    if [[ -z "$minute" ]] || [[ -z "$hour" ]]; then
+        echo "Invalid input, please enter both minute and hour."
+        continue
+    fi
+
+    if ! [[ "$minute" =~ ^[0-9]+$ ]] || ! [[ "$hour" =~ ^[0-9]+$ ]]; then
+        echo "Input must be a number."
+    elif (( minute < 0 || minute > 59 )) || (( hour < 0 || hour > 23 )); then
+        echo "Invalid range: Minute must be 0-59 and Hour must be 0-23."
     else
-        echo "Invalid input, please enter a valid cronjob format (minutes and hours, e.g: 0 6 or 30 12)"
+        # Exact time of day or interval logic
+        if [[ "$minute" == "0" ]] && [[ "$hour" == "0" ]]; then
+            cron_time="0 0 * * *"
+        elif [[ "$minute" == "0" ]]; then
+            cron_time="0 */${hour} * * *"
+        elif [[ "$hour" == "0" ]]; then
+            cron_time="*/${minute} * * * *"
+        else
+            cron_time="${minute} ${hour} * * *"
+        fi
+        break
     fi
 done
 
-
+# --- Panel Selection (Added '3' for 3XUI-Multi) ---
 
 while [[ -z "$xmhs" ]]; do
-    echo "x-ui or s-ui or marzban or hiddify? [x/s/m/h] : "
+    echo "x-ui or s-ui or marzban or hiddify or multi-x-ui (t) or 3xui-multi (3)? [x/s/m/h/t/3] : "
     read -r xmhs
-    if [[ $xmhs == $'\0' ]]; then
-        echo "Invalid input. Please choose x,s, m or h."
+    if [[ -z "$xmhs" ]]; then
+        echo "Invalid input. Please choose x, s, m, h, t, or 3."
         unset xmhs
-    elif [[ ! $xmhs =~ ^[xmhs]$ ]]; then
-        echo "${xmhs} is not a valid option. Please choose x, m or h."
+    elif [[ ! $xmhs =~ ^[xsmht3]$ ]]; then
+        echo "${xmhs} is not a valid option. Please choose x, s, m, h, t, or 3."
         unset xmhs
     fi
 done
@@ -65,7 +76,7 @@ done
 while [[ -z "$crontabs" ]]; do
     echo "Would you like the previous crontabs to be cleared? [y/n] : "
     read -r crontabs
-    if [[ $crontabs == $'\0' ]]; then
+    if [[ -z "$crontabs" ]]; then
         echo "Invalid input. Please choose y or n."
         unset crontabs
     elif [[ ! $crontabs =~ ^[yn]$ ]]; then
@@ -79,34 +90,36 @@ sudo crontab -l | grep -vE '/root/ac-backup.+\.sh' | crontab -
 fi
 
 
+# --- Backup Logic ---
+
 if [[ "$xmhs" == "m" ]]; then
+# --- Marzban Logic ---
+    if dir=$(find /opt /root -type d -iname "marzban" -print -quit); then
+        echo "The folder exists at $dir"
+    else
+        echo "The folder does not exist."
+        exit 1
+    fi
 
-if dir=$(find /opt /root -type d -iname "marzban" -print -quit); then
-  echo "The folder exists at $dir"
-else
-  echo "The folder does not exist."
-  exit 1
-fi
+    if [ -d "/var/lib/marzban/mysql" ] || [ -d "/var/lib/mysql/marzban" ]; then
 
-if [ -d "/var/lib/marzban/mysql" ] || [ -d "/var/lib/mysql/marzban" ]; then
+        path=""
 
-path=""
+        if [ -d "/var/lib/marzban/mysql" ]; then
+            path="/var/lib/marzban/mysql"
+        elif [ -d "/var/lib/mysql/marzban" ]; then
+            path="/var/lib/mysql/marzban"
+        else
+            echo "Neither path exists."
+            exit 1
+        fi
 
-if [ -d "/var/lib/marzban/mysql" ]; then
-  path="/var/lib/marzban/mysql"
-elif [ -d "/var/lib/mysql/marzban" ]; then
-  path="/var/lib/mysql/marzban"
-else
-  echo "Neither path exists."
-  exit 1
-fi
+        sed -i -e 's/\s*=\s*/=/' -e 's/\s*:\s*/:/' -e 's/^\s*//' /opt/marzban/.env
 
-  sed -i -e 's/\s*=\s*/=/' -e 's/\s*:\s*/:/' -e 's/^\s*//' /opt/marzban/.env
+        docker exec marzban-mysql-1 bash -c "mkdir -p /var/lib/mysql/db-backup"
+        source /opt/marzban/.env
 
-  docker exec marzban-mysql-1 bash -c "mkdir -p /var/lib/mysql/db-backup"
-  source /opt/marzban/.env
-
-    cat > "$path/ac-backup.sh" <<EOL
+        cat > "$path/ac-backup.sh" <<EOL
 #!/bin/bash
 
 USER="root"
@@ -118,16 +131,15 @@ databases=\$(mysql -h 127.0.0.1 --user=\$USER --password=\$PASSWORD -e "SHOW DAT
 for db in \$databases; do
     if [[ "\$db" != "information_schema" ]] && [[ "\$db" != "mysql" ]] && [[ "\$db" != "performance_schema" ]] && [[ "\$db" != "sys" ]] ; then
         echo "Dumping database: \$db"
-		mysqldump -h 127.0.0.1 --force --opt --user=\$USER --password=\$PASSWORD  --routines --databases \$db > /var/lib/mysql/db-backup/\$db.sql
+        mysqldump -h 127.0.0.1 --force --opt --user=\$USER --password=\$PASSWORD  --routines --databases \$db > /var/lib/mysql/db-backup/\$db.sql
 
     fi
 done
 
 EOL
-chmod +x "$path/ac-backup.sh"
+        chmod +x "$path/ac-backup.sh"
 
-ZIP=$(cat <<EOF
-
+        ZIP=$(cat <<EOF
 docker exec marzban-mysql-1 bash -c "/var/lib/mysql/ac-backup.sh"
 zip -r /root/ac-backup-m.zip /opt/marzban/* /var/lib/marzban/* /opt/marzban/.env -x $path/\*
 zip -r /root/ac-backup-m.zip $path/db-backup/*
@@ -136,66 +148,87 @@ EOF
 )
 
     else
-      ZIP="zip -r /root/ac-backup-m.zip ${dir}/* /var/lib/marzban/* /opt/marzban/.env"
-fi
+        ZIP="zip -r /root/ac-backup-m.zip ${dir}/* /var/lib/marzban/* /opt/marzban/.env"
+    fi
 
-ACLover="marzban backup"
+    ACLover="marzban backup"
 
-elif [[ "$xmhs" == "x" || "$xmhs" == "s" ]]; then
+elif [[ "$xmhs" == "x" || "$xmhs" == "s" || "$xmhs" == "t" ]]; then
+# --- X-UI/S-UI/Multi X-UI Logic ---
 
-ACLover=""
-dbDir=$(find /etc /opt/freedom /usr/local \
-           -type d \( -iname "x-ui*" -o -iname "s-ui" \) \
-           -print -quit 2>/dev/null)
+    ACLover=""
+    dbDir=$(find /etc /opt/freedom /usr/local -type d \( -iname "x-ui*" -o -iname "s-ui" -o -iname "multi-x-ui" \) -print -quit 2>/dev/null)
 
 
-if [[ -n "${dbDir}" ]]; then
-  echo "The folder exists at $dbDir"
-  if [[ $dbDir == "/opt/freedom/x-ui"* ]]; then
-    dbDir="${dbDir}/db/x-ui.db"
-    ACLover="x-ui backup"
-  elif [[ $dbDir == "/usr/local/s-ui" ]]; then
-    dbDir="${dbDir}/db/s-ui.db" 
-    ACLover="s-ui backup"
-  else
-    dbDir="${dbDir}/x-ui.db"
-    ACLover="x-ui backup"
-  fi
-else
-  echo "The folder does not exist."
-  exit 1
-fi
+    if [[ -n "${dbDir}" ]]; then
+        echo "The folder exists at $dbDir"
+        if [[ $dbDir == "/opt/freedom/x-ui"* ]]; then
+            dbDir="${dbDir}/db/x-ui.db"
+            ACLover="x-ui backup"
+        elif [[ $dbDir == "/usr/local/s-ui" ]]; then
+            dbDir="${dbDir}/db/s-ui.db"
+            ACLover="s-ui backup"
+        elif [[ $dbDir == "/usr/local/multi-x-ui" ]]; then
+            dbDir="${dbDir}/db/multi-x-ui.db"
+            ACLover="multi-x-ui backup"
+        else
+            dbDir="${dbDir}/x-ui.db"
+            ACLover="x-ui backup"
+        fi
+    else
+        echo "The folder does not exist."
+        exit 1
+    fi
 
-configDir=$(find /usr/local -type d -iname "x-ui*" -print -quit 2>/dev/null)
-if [[ -n "${configDir}" ]]; then
-  echo "The folder exists at $configDir"
-  configDir="${configDir}/config.json"
-else
-  configDir=""
-fi
+    configDir=$(find /usr/local -type d \( -iname "x-ui*" -o -iname "s-ui" -o -iname "multi-x-ui" \) -print -quit 2>/dev/null)
+    if [[ -n "${configDir}" ]]; then
+        echo "The folder exists at $configDir"
+        if [[ $configDir == "/usr/local/multi-x-ui" ]]; then
+            configDir="${configDir}/config.json"
+        elif [[ $configDir == "/usr/local/s-ui" ]]; then
+            configDir="${configDir}/config.json"
+        else
+            configDir="${configDir}/config.json"
+        fi
+    else
+        configDir=""
+    fi
 
-ZIP="zip /root/ac-backup-${xmhs}.zip ${dbDir} ${configDir}"
+    ZIP="zip /root/ac-backup-${xmhs}.zip ${dbDir} ${configDir}"
+
+elif [[ "$xmhs" == "3" ]]; then
+# --- 3XUI-Multi Logic ---
+
+    targetDir="/opt/3xui-multi"
+
+    if [ ! -d "$targetDir" ]; then
+        echo "The folder ${targetDir} does not exist."
+        exit 1
+    fi
+
+    ZIP="zip -r /root/ac-backup-3.zip ${targetDir}/"
+    ACLover="3xui-multi backup"
 
 
 elif [[ "$xmhs" == "h" ]]; then
+# --- Hiddify Logic ---
+    if ! find /opt/hiddify-manager/hiddify-panel/ -type d -iname "backup" -print -quit; then
+        echo "The folder does not exist."
+        exit 1
+    fi
 
-if ! find /opt/hiddify-manager/hiddify-panel/ -type d -iname "backup" -print -quit; then
-  echo "The folder does not exist."
-  exit 1
-fi
-
-if [ -f "/opt/hiddify-manager/hiddify-panel/backup.sh" ]; then
-  backupCommand="bash backup.sh"
-else
-backupCommand="python3 -m hiddifypanel backup"
-fi
+    if [ -f "/opt/hiddify-manager/hiddify-panel/backup.sh" ]; then
+        backupCommand="bash backup.sh"
+    else
+        backupCommand="python3 -m hiddifypanel backup"
+    fi
 
 
-ZIP=$(cat <<EOF
+    ZIP=$(cat <<EOF
 
 cd /opt/hiddify-manager/hiddify-panel/
-if [ $(find /opt/hiddify-manager/hiddify-panel/backup -type f | wc -l) -gt 100 ]; then
-  find /opt/hiddify-manager/hiddify-panel/backup -type f -delete
+if [ \$(find /opt/hiddify-manager/hiddify-panel/backup -type f | wc -l) -gt 100 ]; then
+    find /opt/hiddify-manager/hiddify-panel/backup -type f -delete
 fi
 
 $backupCommand
@@ -207,10 +240,10 @@ zip /root/ac-backup-h.zip /opt/hiddify-manager/hiddify-panel/backup/\$latest_fil
 
 EOF
 )
-ACLover="hiddify backup"
+    ACLover="hiddify backup"
 else
-echo "Please choose m or x or s or h only !"
-exit 1
+    echo "Please choose m or x or s or h or t or 3 only !"
+    exit 1
 fi
 
 
@@ -227,7 +260,7 @@ comment=$(echo -e "$caption" | sed 's/<code>//g;s/<\/code>//g')
 comment=$(trim "$comment")
 
 
-sudo apt install zip -y
+sudo apt update && sudo apt install zip -y
 
 cat > "/root/ac-backup-${xmhs}.sh" <<EOL
 #!/bin/bash
@@ -240,7 +273,7 @@ EOL
 
 
 
-{ crontab -l -u root; echo "${cron_time} /bin/bash /root/ac-backup-${xmhs}.sh >/dev/null 2>&1"; } | crontab -u root -
+{ crontab -l -u root 2>/dev/null; echo "${cron_time} /bin/bash /root/ac-backup-${xmhs}.sh >/dev/null 2>&1"; } | crontab -u root -
 
 
 bash "/root/ac-backup-${xmhs}.sh"
